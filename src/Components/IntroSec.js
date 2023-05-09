9567e9c072d756e1ccc042110cc0dc470ecd1ea4
Mohdniyam <niyamul.haque98@gmail.com>
2023-05-09 17:34:43 +0530

    responsive and file add

--- src/Components/IntroSec.js
+++ src/Components/IntroSec.js
@@ -1,14 +1,10 @@
 import React, { useContext, useEffect, useRef, useState } from "react";
-import Iframe from "react-iframe";
 import { Col, Container, Row } from "react-bootstrap";
 import { Button } from "@mui/material";
 import Zoom from "react-reveal/Zoom";
 import Typewriter from "typewriter-effect";
 import { GlobalInfo } from "../context/GlobalContext";
-import intro_bg from "../Assets/intro-bg.svg";
-// import riksha from "../Assets/e-riksha.mp4";
-import finalGif from '../Assets/final_1.gif'
-import "./IntroSec.css";
+import intro_bg from '../Assets/intro-bg.svg';
 
 function IntroSec() {
   const { home, services, whyUs, contactUs, quote } = useContext(GlobalInfo);
@@ -55,34 +51,86 @@ function IntroSec() {
 
   return (
     <>
-      <section className="Section-1">
-        <div className="landing-page">
-          {/* <div className="floating-bg"></div> */}
-          <div className="text-center">
-            <p className="intro-sec-title">
-              Embedding Finance in EV for a Better Tomorrow!{" "}
-            </p>
-            <h1 className="intro-sec-subtitle">
-              <span className="heading-1">Ev Financing</span>
-              <span className="heading-2">Checkout Financing</span>
-              <span className="heading-3">Payment</span>
-            </h1>
-            <Button
-              className="intro-btn ms-0 mt-4 banner-btn"
-              variant="outlined"
-              onClick={() =>
-                quote.current?.scrollIntoView({ behavior: "smooth" })
-              }
-            >
-              Learn more
-            </Button>
-          </div>
-        </div>
-        {/* <div style={{ background: "red", width }} >
-          <video src={riksha} autoPlay loop />
-        </div> */}
-        <img src={finalGif} alt="loading....." style={{width:"100%", height:"40%"}} className="gifImage"/> 
+      <section className="intro-sec section-1 intro_row" ref={home}>
+        <img src={intro_bg} className='intro-bg-img' />
+        <Container className="w-100">
+          <Row className="text-center">
+            <Col md={12}>
+              <div className="text-center">
+                <p className="intro-sec-title">Embedding Finance in EV for a Better Tomorrow! </p>
+                <p className="intro-sec-subtitle">
+                  <Typewriter
+                    onInit={(typewriter) => {
+                      typewriter
+                        .typeString("EV Financing, Checkout Financing, Payments")
+                        .changeDelay(75)
+                        .start();
+                    }}
+                  />
+                </p>
+              </div>
+              <Button
+                className="intro-btn ms-0 mt-4"
+                variant="outlined"
+                onClick={() =>
+                  quote.current?.scrollIntoView({ behavior: "smooth" })
+                }
+              >
+                Learn more
+              </Button>
+            </Col>
+            {/* <Col className="intro-img-col" md={4}>
+              <img
+                src="https://drilca9ckdzo8.cloudfront.net/fit-in/money.png"
+                width="100%"
+                className="vert-move intro-pic"
+                ref={introImg}
+                draggable={false}
+              />
+            </Col> */}
+          </Row>
+        </Container>
+        {/* <div
+          class="scroll-down"
+          style={
+            isScrollIcon
+              ? { opacity: 1, transition: "all 1s" }
+              : { opacity: 0, transition: "all 1s" }
+          }
+        /> */}
       </section>
+
+      {/* <section className="intro-sec bg-dark section-2">
+        <Container>
+          <Row>
+            <Col xs={{ span: 12, order: 2 }} md={{ span: 6, order: 1 }}>
+              <div className="embi-logo">
+                <img src="https://drilca9ckdzo8.cloudfront.net/embi.svg" alt="" />
+              </div>
+              <Zoom>
+                <p className="text-center mt-3">
+                  <span className="mx-3">BUILD</span>|
+                  <span className="mx-3">ENABLE</span>|
+                  <span className="mx-3">EMBED</span>
+                </p>
+              </Zoom>
+            </Col>
+            <Col
+              className="intro-img-col"
+              xs={{ span: 12, order: 1 }}
+              md={{ span: 6, order: 2 }}
+            >
+              <img
+                src={"https://drilca9ckdzo8.cloudfront.net/hand.svg"}
+                width="100%"
+                className="vert-move intro-hand-pic"
+                ref={handImg}
+                draggable={false}
+              />
+            </Col>
+          </Row>
+        </Container>
+      </section> */}
     </>
   );
 }
